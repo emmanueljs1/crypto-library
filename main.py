796e180892294caf95e53fa19c60a8384928e086
Julia Chun <hjchun96@gmail.com>
2017-12-11 23:37:20 -0500

    updated main

--- main.py
+++ main.py
@@ -7,76 +7,99 @@ Created on Sun Dec 10 17:28:53 2017
 """
 
 from flask import Flask, render_template, flash, request
-# from flask_wtf import Form as FlaskForm
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import logging
 from encrypt import Encryptor
 app = Flask(__name__)
-
  
-class EncryptForm(Form):
+encryption_algorithm = ""
+decryption_algorithm = ""
 
-    text = TextField('Text to encrypt', validators=[validators.Required()])
-   
-class DecryptForm(Form):
-
-    text = TextField('Text to Decrypt', validators=[validators.Required()]) 
-
- 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/process_form", methods=['GET', 'POST'])
 def process_form():
     
-    encrypt_form = EncryptForm(request.form)
-    decrypt_form = DecryptForm(request.form)
-    # encrypt_form = EncryptForm()
-    # decrypt_form = DecryptForm()
+    decrypt_alg = "LFSR"
+    encrypt_alg = "LFSR"
 
     if request.method == 'POST':
+        request_dict = request.form.to_dict()
+
+        if 'encryption_select' in request.form:
+            encrypt_alg = request_dict['encryption_select']
+ 
+        elif 'decryption_select' in request.form:
+            decrypt_alg = request_dict['decryption_select']
+    
+    crypto_list = ['LFSR', 'AES', 'Symmetric Authenticated Cryptography']
+
+    # update global variable
+    encryption_algorithm = encrypt_alg
+    decryption_algorithm = decrypt_alg
+
+    return render_template('index.html', encrypt_alg=encrypt_alg, decrypt_alg=decrypt_alg, crypto_list=crypto_list)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def encrypt():
+    
+    if request.method == 'POST':
+        request_dict = equest.form.to_dict()
 
         if 'encrypt' in request.form:
 
-            encrypt_dict = request.form.to_dict()
-            text = encrypt_dict['encrypt']
-            print(text)
-
+            text = request_dict['encrypt']
             encryptor = Encryptor()
-            encrypted_text = encryptor.encrypt_LFSR(text)
-            seed = encryptor.seed
-            tap = encryptor.tap
+            encrypted_text = ""
 
-            # we need to give the user the seed and tap as well
+            if encryption_algorithm is "LFSR":
             
-            # TODO: ask the user if they want to encrypt with AES or 
-            # symmetric authenticated cryptography
+                encrypted_text = encryptor.encrypt_LFSR(text)
+                seed = encryptor.seed
+                tap = encryptor.tap
 
-            # if encrypt_form.validate():
-            if len(text) > 0:
-                flash('Encryption Result: {}\n Seed:\n {} Tap: {} '.format(encrypted_text, seed, tap) , 'encrypt')
+                if len(text) > 0:
+                    flash('Encryption Result: {}\n Seed:{} \n Tap: {} \n '.format(encrypted_text, seed, tap) , 'encrypt')
+                else:
+                    flash('Error: All the form fields are required. ', 'encrypt')
+
+            elif encryption_algorithm is "AES":
+    
+                encrypted_text = encryptor.encrypt_AES(text)
+                key = encryptor.AES_key
+                if len(text) > 0:
+                    flash('Encryption Result: {}\n Key: {}\n '.format(encrypted_text, key) , 'encrypt')
+                else:
+                    flash('Error: All the form fields are required. ', 'encrypt')
+
             else:
-                flash('Error: All the form fields are required. ', 'encrypt')
-            decrypt_form = None
+
+                encrypted_text = encryptor.encrypt(text)
+                key = encryptor.key
+
+                if len(text) > 0:
+                    flash('Encryption Result: {}\n Seed:\n {} Tap: {} '.format(encrypted_text, key) , 'encrypt')
+                else:
+                    flash('Error: All the form fields are required. ', 'encrypt')
+    
         else:
             
-            decrypt_dict = request.form.to_dict()
-            text = decrypt_dict['decrypt']
+            text = request_dict['decrypt']
             print(text)
-
-            # TODO: ask the user if they want to decrypt with AES or 
-            # symmetric authenticated cryptography
 
             seed = '' # user needs to provide seed online as well
             tap = 0 # user needs to provide tap position as well
             
             decrypted = Encryptor.decrypt_with_lfsr(seed, tap, text)
-
+            decrypt_form = decrypt_form2
             # if decrypt_form.validate():
             if len(text) > 0:
                 flash('Decryption Result:' + decrypted, 'decrypt')
             else:
                 flash('Error: All the form fields are required. ', 'decrypt')
             encrypt_form = None
- 
-    return render_template('index.html', encrypt_form=encrypt_form, decrypt_form=decrypt_form)
+    
+    crypto_list = ['LFSR', 'AES', 'Symmetric Authenticated Cryptography']
+    return render_template('index.html', decrypt_alg=decrypt_alg, crypto_list=crypto_list)
 
 
 def main():
